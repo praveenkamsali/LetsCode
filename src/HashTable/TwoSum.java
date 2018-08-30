d34f7ea0762ad8365321bff8d4b3c7403692ad0e
Ramya Sree <praveenkamsali@gmail.com>
2018-08-29 22:37:43 -0500

    TwoSum Problem - Finding the elements in an array with a defined sum of two elements

--- src/HashTable/TwoSum.java
+++ src/HashTable/TwoSum.java
@@ -1,7 +1,30 @@
 package HashTable;
 
+import java.util.Hashtable;
+
 /**
  * Created by praveenkamsali on 8/29/18.
  */
 public class TwoSum {
+
+    public static void main(String[] args){
+        int[] nums = new int[]{1,2,3,4,5};
+        int[] result = twoSumLogic(nums,1);
+        for(int i=0;i<result.length;i++) {
+            System.out.println("The Target Values are : " + result[i]);
+        }
+    }
+
+    private static int[] twoSumLogic(int[] nums,int target) {
+        Hashtable<Integer,Integer> table = new Hashtable<Integer,Integer>();
+        for(int i =0;i<=nums.length;i++){
+          int complement = target - nums[i];
+            if(table.containsKey(complement)){
+                return new int[]{i,table.get(complement)};
+            }
+            table.put(nums[i],i);
+        }
+
+        throw new IllegalArgumentException("Match not Found");
+    }
 }

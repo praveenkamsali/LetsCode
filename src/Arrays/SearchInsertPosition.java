84957fe7fcefe34e6652fdd0424b4e2c9e450d1b
Ramya Sree <praveenkamsali@gmail.com>
2018-12-02 20:28:39 -0600

    Search Insert Position Problem - Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.

--- src/Arrays/SearchInsertPosition.java
+++ src/Arrays/SearchInsertPosition.java
@@ -3,6 +3,10 @@ package Arrays;
 /**
  * Created by praveenkamsali on 12/2/18.
  */
+/*
+* Given a sorted array and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+* */
 public class SearchInsertPosition {
     public static void main(String[] args) {
         int[] nums = {1, 3, 5, 6,7,9,13,57};
